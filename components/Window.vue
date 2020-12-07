53d44c9d2ac82fca51956f5738790361409fda18
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2020-12-07 03:58:46 -0300

    add resize

--- components/Window.vue
+++ components/Window.vue
@@ -1,12 +1,10 @@
 <template>
-  <div
-    class="window"
-    :style="{top, left, width, height}"
-  >
-    <div
-      class="bar"
-    @mousedown="mouseDown"
-    ></div>
+  <div class="window" :style="{top, left, width, height}">
+    <div class="resizer nw" @mousedown="handleResize" />
+    <div class="resizer ne" @mousedown="handleResize" />
+    <div class="resizer sw" @mousedown="handleResize" />
+    <div class="resizer se" @mousedown="handleResize" />
+    <div class="bar" @mousedown="handleDrag" />
   </div>
 </template>
 
@@ -22,20 +20,17 @@ export default {
       type: String,
       required: false,
       default: '200px'
-    },
-    top: {
-      type: String,
-      required: false,
-      default: '200px'
-    },
-    left: {
-      type: String,
-      required: false,
-      default: '200px'
+    }
+  },
+  data: () => {
+    return {
+      left: '50px',
+      top: '100px',
+      isResizing: false
     }
   },
   methods: {
-    mouseDown (e) {
+    handleDrag (e) {
       let prevX = e.clientX
       let prevY = e.clientY
 
@@ -44,8 +39,8 @@ export default {
         const newY = prevY - e.clientY
         const rect = this.$el.getBoundingClientRect()
 
-        this.$el.style.left = rect.left - newX + 'px'
-        this.$el.style.top = rect.top - newY + 'px'
+        this.left = rect.left - newX + 'px'
+        this.top = rect.top - newY + 'px'
 
         prevX = e.clientX
         prevY = e.clientY
@@ -54,6 +49,47 @@ export default {
         window.removeEventListener('mousemove', mouseMove)
         window.removeEventListener('mouseup', mouseUp)
       }
+      if (!this.isResizing) {
+        window.addEventListener('mousemove', mouseMove)
+        window.addEventListener('mouseup', mouseUp)
+      }
+    },
+    handleResize (e) {
+      this.isResizing = true
+      const vertice = e.target
+
+      let prevX = e.clientX
+      let prevY = e.clientY
+
+      const mouseMove = (e) => {
+        const rect = this.$el.getBoundingClientRect()
+
+        if (vertice.classList.contains('se')) {
+          this.$el.style.width = rect.width - (prevX - e.clientX) + 'px'
+          this.$el.style.height = rect.height - (prevY - e.clientY) + 'px'
+        } else if (vertice.classList.contains('sw')) {
+          this.$el.style.width = rect.width + (prevX - e.clientX) + 'px'
+          this.$el.style.height = rect.height - (prevY - e.clientY) + 'px'
+          this.$el.style.left = rect.left - (prevX - e.clientX) + 'px'
+        } else if (vertice.classList.contains('ne')) {
+          this.$el.style.width = rect.width - (prevX - e.clientX) + 'px'
+          this.$el.style.height = rect.height + (prevY - e.clientY) + 'px'
+          this.$el.style.top = rect.top - (prevY - e.clientY) + 'px'
+        } else {
+          this.$el.style.width = rect.width + (prevX - e.clientX) + 'px'
+          this.$el.style.height = rect.height + (prevY - e.clientY) + 'px'
+          this.$el.style.top = rect.top - (prevY - e.clientY) + 'px'
+          this.$el.style.left = rect.left - (prevX - e.clientX) + 'px'
+        }
+
+        prevX = e.clientX
+        prevY = e.clientY
+      }
+      const mouseUp = () => {
+        window.removeEventListener('mousemove', mouseMove)
+        window.removeEventListener('mouseup', mouseUp)
+        this.isResizing = false
+      }
       window.addEventListener('mousemove', mouseMove)
       window.addEventListener('mouseup', mouseUp)
     }
@@ -61,7 +97,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
   .window{
     border: black 1px solid;
     position: absolute;
@@ -69,5 +105,32 @@ export default {
   .bar{
     height: 20px;
     background-color: brown;
+  }
+  .resizer{
+    position: absolute;
+    width: 5px;
+    height: 5px;
+    border-radius: 5px;
+    background: black;
+  }
+  .resizer.nw{
+    top: -1px;
+    left: -1px;
+    cursor: nw-resize;
+  }
+  .resizer.ne{
+    top: -1px;
+    right: -1px;
+    cursor: ne-resize;
+  }
+  .resizer.sw{
+    bottom: -1px;
+    left: -1px;
+    cursor: sw-resize;
+  }
+  .resizer.se{
+    bottom: -1px;
+    right: -1px;
+    cursor: se-resize;
   }
 </style>
